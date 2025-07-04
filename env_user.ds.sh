ade821951d6d7ae322af272c9b008de31b434690
Valeriy Onuchin <40940665+x2v0@users.noreply.github.com>
2025-07-04 15:33:07 +0300

    Update env_user.ds.sh

--- env_user.ds.sh
+++ env_user.ds.sh
@@ -2,8 +2,7 @@
 
 # Create ~/.bash_profile
 cat > ~/.bash_profile << "EOF"
-exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' 
-/bin/bash
+exec env -i HOME=$HOME TERM=$TERM PS1='\u:\w\$ ' /bin/bash
 EOF
 
 # Create ~/.bashrc
