3fa26cfeca83eec17557aff2a957791df5f653d8
Valeriy Onuchin <40940665+x2v0@users.noreply.github.com>
2025-07-04 15:25:02 +0300

    Update create_user.sh

--- create_user.sh
+++ create_user.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-[ ! -e /etc/bash.bashrc ] || sudo mv -v /etc/bash.bashrc /etc/bash.bashrc.NOUSE
+[ ! -e /etc/bash.bashrc ] || mv -v /etc/bash.bashrc /etc/bash.bashrc.NOUSE
 
 exist=$(getent passwd | cut -f1 -d: | grep -w "lfs")
 
 if [ -z "$exist" ]; then
-	sudo groupadd lfs
-	sudo useradd -s /bin/bash -g lfs -m -k /dev/null lfs
+	groupadd lfs
+	useradd -s /bin/bash -g lfs -m -k /dev/null lfs
 	echo -e "lfs\nlfs" | sudo passwd lfs
 	echo "lfs ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 fi
 
 cp -rf *.sh .bash* md5sums wget-list Chapter* /home/lfs/
 
-sudo su - lfs
+su - lfs
